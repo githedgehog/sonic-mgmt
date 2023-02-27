4e2cf0fa30fb67864d3d83388233ff4c9fec2906
AndriiDovhan <andrii@githedgehog.com>
2023-02-27 16:16:00 +0200

    Add script for env setup and test run (#26)

---
+++ tests/hedgehog/script/jenkins_helper.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+BRANCH=''
+SERVER=''
+TESTBED=''
+REPORT_DIR=''
+SONIC_IMG=''
+ALLURE_TOKEN=''
+
+OPTIND=1
+while getopts ':b:s:t:r:i:a:' flag; do
+  case "${flag}" in
+    b) BRANCH="${OPTARG}" ;;
+    s) SERVER="${OPTARG}" ;;
+    t) TESTBED="${OPTARG}" ;;
+    r) REPORT_DIR="${OPTARG}" ;;
+    i) SONIC_IMG="${OPTARG}" ;;
+    a) ALLURE_TOKEN="${OPTARG}" ;;
+  esac
+done
+
+SONIC_MGMT_WD="/home/hedgehog/sonic-mgmt"
+SSH_OPTIONS="-o ServerAliveInterval=30 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+MGMT_CONTAINER=`ssh -q $SSH_OPTIONS $SERVER "docker ps -f ancestor=docker-sonic-mgmt-hedgehog:master --format {{.Names}}"`
+REPORT_PREFIX=`ssh -q $SSH_OPTIONS $SERVER "cat $SONIC_MGMT_WD/tests/$TESTBED | yq .testbed.report_base_dir"`
+DUT_IP=`ssh -q $SSH_OPTIONS $SERVER "cat $SONIC_MGMT_WD/tests/$TESTBED | yq .testbed.dut_ip"`
+REPORT_DIR="$(date +%Y%m%d)-$REPORT_DIR"
+
+
+redeployEnv() {
+    if [ ! -f "$SONIC_IMG" ]; then
+        echo "$SONIC_IMG does not exist."
+        exit 1
+    fi
+
+    echo "Deploy topology"
+    ssh $SSH_OPTIONS $SERVER "cd $SONIC_MGMT_WD && git stash; git checkout $BRANCH; git pull"
+    ssh $SSH_OPTIONS $SERVER "cd $SONIC_MGMT_WD/ansible && ./hedgehog_deployment_script.sh"
+
+    DUT_USER=`ssh -q $SSH_OPTIONS $SERVER "cat $SONIC_MGMT_WD/ansible/group_vars/lab/secrets.yml | yq .sonicadmin_user"`
+    DUT_PASS=`ssh -q $SSH_OPTIONS $SERVER "cat $SONIC_MGMT_WD/ansible/group_vars/lab/secrets.yml | yq .sonicadmin_password"`
+
+    echo "Copy SONiC image to DUT"
+    sshpass -p $DUT_PASS scp $SSH_OPTIONS -o "ProxyCommand ssh $SSH_OPTIONS $SERVER -W %h:%p" $SONIC_IMG $DUT_USER@$DUT_IP:/home/admin/
+
+    echo "Install SONiC image"
+    sshpass -p $DUT_PASS ssh $SSH_OPTIONS -o "ProxyCommand ssh $SSH_OPTIONS $SERVER -W %h:%p" $DUT_USER@$DUT_IP "sudo sonic-installer install -y /home/admin/$SONIC_IMG"
+
+    echo "Reboot DUT to apply changes"
+    sshpass -p $DUT_PASS ssh $SSH_OPTIONS -o "ProxyCommand ssh $SSH_OPTIONS $SERVER -W %h:%p" $DUT_USER@$DUT_IP "sudo reboot"
+
+    echo "Apply hedgehog user patch"
+    ssh $SSH_OPTIONS $SERVER "cd $SONIC_MGMT_WD && git apply test_hedgehog_user.patch"
+}
+
+runTests() {
+    DUT_USER=`ssh -q $SSH_OPTIONS $SERVER "cat $SONIC_MGMT_WD/ansible/group_vars/lab/secrets.yml | yq .sonicadmin_user"`
+    DUT_PASS=`ssh -q $SSH_OPTIONS $SERVER "cat $SONIC_MGMT_WD/ansible/group_vars/lab/secrets.yml | yq .sonicadmin_password"`
+
+    launch_name=`sshpass -p $DUT_PASS ssh $SSH_OPTIONS -o "ProxyCommand ssh $SSH_OPTIONS $SERVER -W %h:%p" $DUT_USER@$DUT_IP "show version" | grep -oP '(?<=SONiC Software Version: SONiC\.).*'`
+    echo $launch_name
+
+    echo "Run test cases"
+    ssh $SSH_OPTIONS $SERVER "docker exec -t $MGMT_CONTAINER bash -c \"cd /data/sonic-mgmt/tests && python3.8 hedgehog_test_runner.py \
+                --testbed $TESTBED --report_dir_name $REPORT_DIR --launch_name $launch_name --allurectl_token $ALLURE_TOKEN \" "
+}
+
+copyArtifacts() {
+    echo "Copy allure and pytest reports"
+    prefix=${REPORT_PREFIX#"/data/sonic-mgmt/"}
+    mkdir -p reports
+    scp -r $SSH_OPTIONS $SERVER:$SONIC_MGMT_WD/$prefix/$REPORT_DIR reports/
+    scp -r $SSH_OPTIONS $SERVER:$SONIC_MGMT_WD/tests/report.html reports/
+}
